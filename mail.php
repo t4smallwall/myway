5797a2080b5d9791932e5520b6b566b0937a331e
t4smallwall <cozyfrank@gmail.com>
2012-03-15 19:26:15 +0530

    updated php mail.php for a missing semicolon and $value typo.

--- mail.php
+++ mail.php
@@ -67,7 +67,7 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers); 
-$vaue = "Thank you for contacting us. We will be in touch with you very soon."
+$value = "Thank you for contacting us. We will be in touch with you very soon.";
 setcookie("mailcookie",$value, time()+120);
 ?>
 <!-- include your own success html here -->
